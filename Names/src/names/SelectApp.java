972f59c5883a6f9662e4889d2258d7e1260386ad
Mark Ferguson <maggot.iiss@sky.com>
2018-09-13 07:02:39 +0100

    20180913

--- Names/src/names/SelectApp.java
+++ Names/src/names/SelectApp.java
@@ -9,96 +9,65 @@ package names;
  *
  * @author Mark Ferguson
  */
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author sqlitetutorial.net
  */
 public class SelectApp {
-
-    /**
-     * Connect to the test.db database
-     *
-     * @return the Connection object
-     */
-    private Connection connect() {
-        // SQLite connection string
-        String url = "jdbc:sqlite:C:/sqlite/names.db";
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(url);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return conn;
-    }
-
-    /**
-     * select all rows in the table
-     */
-    public void selectAll(String tablename) {
-        String sql = "SELECT * FROM " + tablename;
-        try (Connection conn = this.connect();
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery(sql)) {
-            // loop through the result set
-            while (rs.next()) {
-                System.out.println(rs.getString("name"));
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    public void selectOne(String tablename, int num) {
-        String sql = "SELECT * FROM " + tablename;
-        int count = 0;
-        try (Connection conn = this.connect();
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery(sql)) {
-            // loop through the result set
-            rs.next();
-            while (count < num) {
-                if (count == (num - 1)) {
-                    System.out.println(rs.getString("name"));
-                }
-                count += 1;
-                rs.next();
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    public String randomOne(String tablename) {
-        String sql = "SELECT * FROM " + tablename + " ORDER BY RANDOM() LIMIT 1";
-        int count = 0;
-        try (Connection conn = this.connect();
-                Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery(sql)) {
-            //System.out.println(rs.getString("name"));
-            return (rs.getString("name"));
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return null;
-    }
+    //path to this folder on hd, but can be changed (public)
+    public static String path = "C:/GitHub/Names/Names/src/names/";
+    private static String CRLF = "\n";
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        int num = 3;
+    public static void main(String[] args) throws FileNotFoundException {
         SelectApp app = new SelectApp();
-//        app.selectAll("humaniti_male_first");
+        String line = app.getCouple("humaniti");
+        savePage(line);
+//        int num = 3;
 //        app.selectOne("humaniti_female_first", num);
+//        app.selectAll("humaniti_male_first");
 //        app.randomOne("humaniti_female_first");
-        System.out.println(app.getCouple("humaniti"));
+    }
+
+    private static void savePage(String name) throws FileNotFoundException {
+        try {
+            System.out.println(name);
+            String page = replaceHTML("test.html",name, name+".html");
+            System.out.println(page);
+        } catch (IOException ex) {
+            Logger.getLogger(SelectApp.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private static String replaceHTML(String fname, String insert, String output) throws FileNotFoundException, IOException {
+        BufferedReader br = new BufferedReader(new FileReader(path + fname));
+        String line = "";
+        String page = "";
+        while((line = br.readLine()) != null ) {
+            line = line.replace("REPLACE", insert);
+            page += line + CRLF;
+        }
+        br.close();
+        FileWriter fw = new FileWriter(path + output);
+        fw.write(page);
+        fw.close();
+        return page;
     }
 
     private String getCouple(String race) {
@@ -127,4 +96,73 @@ public class SelectApp {
         String Couple = names[0] + " & " + names[1] + " " + names[2];
         return Couple;
     }
+
+    public String randomOne(String tablename) {
+        String sql = "SELECT * FROM " + tablename + " ORDER BY RANDOM() LIMIT 1";
+        try (Connection conn = this.connect();
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql)) {
+            return (rs.getString("name"));
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * Connect to the names.db database
+     *
+     * @return the Connection object
+     */
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C:/sqlite/names.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    //==================== COMMENTED OUT ==========================
+    //<editor-fold defaultstate="collapsed" desc="OLD">
+    //    /**
+    //     * select all rows in the table
+    //     */
+    //    public void selectAll(String tablename) {
+    //        String sql = "SELECT * FROM " + tablename;
+    //        try (Connection conn = this.connect();
+    //                Statement stmt = conn.createStatement();
+    //                ResultSet rs = stmt.executeQuery(sql)) {
+    //            // loop through the result set
+    //            while (rs.next()) {
+    //                System.out.println(rs.getString("name"));
+    //            }
+    //        } catch (SQLException e) {
+    //            System.out.println(e.getMessage());
+    //        }
+    //    }
+    //
+    //    public void selectOne(String tablename, int num) {
+    //        String sql = "SELECT * FROM " + tablename;
+    //        int count = 0;
+    //        try (Connection conn = this.connect();
+    //                Statement stmt = conn.createStatement();
+    //                ResultSet rs = stmt.executeQuery(sql)) {
+    //            // loop through the result set
+    //            rs.next();
+    //            while (count < num) {
+    //                if (count == (num - 1)) {
+    //                    System.out.println(rs.getString("name"));
+    //                }
+    //                count += 1;
+    //                rs.next();
+    //            }
+    //        } catch (SQLException e) {
+    //            System.out.println(e.getMessage());
+    //        }
+    //    }
+    //</editor-fold>
 }
